0124e2c62e7f5d58a56b6e338aa613832aa87305
Youssef Shebl <youssefshebl96@gmail.com>
2023-02-26 15:39:29 +0200

    [UPD] bi_mrp_costing

--- bi_mrp_costing/models/mrp_production_inherit.py
+++ bi_mrp_costing/models/mrp_production_inherit.py
@@ -72,7 +72,8 @@ class MRPProduction(models.Model):
                     move_lines.append((0, 0, {
                         'name': rec.display_name,
                         'account_id': account,
-                        'analytic_distribution': {str(analytic_accounts[account]): 100},
+                        'analytic_distribution': {str(analytic_accounts[account]): 100} if analytic_accounts[
+                            account] else False,
                         'credit': accounts[account],
                         'debit': 0,
                     }))
