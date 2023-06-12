5c87a3f5f023b2b8056b0c31e7b4df2c58d0e6be
Asya El-husseini <asya@bisolutions.tech>
2023-06-12 14:36:02 +0300

    [ADD] bi_account_payment_customization

---
+++ bi_account_payment_customization/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BI Account Payment Customization",
+    'summary': "BI Account Payment Customization",
+    'description': """ 
+     """,
+    'author': "BI Solutions Development Team",
+    'category': 'Accounting',
+    'version': '0.1',
+    'depends': ['base', 'account'],
+    'data': [
+        'views/account_payment_views_inherit.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'sequence': 1
+}
